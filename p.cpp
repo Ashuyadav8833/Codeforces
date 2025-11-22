63b4c1971684790827046db3f7cb63b815e32b51
Ashuyadav8833 <ashu8833yadav@gmail.com>
2025-11-23 00:07:56 +0530

    first commit

---
+++ p.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num;
+
+    while(num>=10) {
+        num /= 10;
+    }
+
+    if(num % 2 == 0)
+        cout << "EVEN";
+    else
+        cout << "ODD";
+
+    return 0;
+}
